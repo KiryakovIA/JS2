e4991bdf0a08429a29d21c5e969e6c4fbedf785f
KiryakovIA <48943805+KiryakovIA@users.noreply.github.com>
2019-05-07 08:15:51 +0300

    1

--- Lesson_1/js/script.js
+++ Lesson_1/js/script.js
@@ -1,5 +1,20 @@
-const clr = '<div class="clr"></div>';
 const goods = [
+	{
+		title: 'Shirt',
+		price: 150
+	},
+	{
+		title: 'Socks',
+		price: 50
+	},
+	{
+		title: 'Jacket',
+		price: 350
+	},
+	{
+		title: 'Shoes',
+		price: 250
+	},
 	{
 		title: 'Shirt',
 		price: 150
@@ -24,7 +39,7 @@ const renderGoodsItem = (title, price = 0) => {
 
 const renderGoodsList = (list) => {
 	let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-	document.querySelector('.goods-list').innerHTML = goodsList.join('') + clr;
+	document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
 renderGoodsList(goods);
